5be169817846a4d5c5f41ad3f8b2acdbd834e617
Stark Industries <subhambudhathoki007@gmail.com>
2021-03-09 09:41:27 +0545

    program to find BMI

---
+++ practise/mass.py
@@ -0,0 +1,8 @@
+#a persons body mass is defined as
+#BMI=mass in kg / (height in m)2
+
+
+mass=int(input("enter the mass of the person"))
+height=float(input("enter the height of the person"))
+BMI=(mass/height*height)
+print("the BMI is given as",BMI)
